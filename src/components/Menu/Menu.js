26a79a67358b7c31733b532957c97555b71cbe40
JorgeRuizParra <zx20student122@colegiovalledelmiro.es>
2024-12-15 17:11:10 +0100

    Curso

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,81 @@
+import React, { Component, useRef } from 'react'
+import style from './menu.css'
+import logo from '../../assets/images/logoTajamar.png'
+import curso from '../../assets/images/educacion.png'
+import { NavLink } from 'react-router-dom'
+import Global from '../../Global'
+import axios from 'axios'
+
+const Menu = () => {
+
+    const barraMenu = useRef(null);
+    const txtIcon = useRef(null);
+    const icon = useRef(null);
+
+    const ampliarMenu = () => {
+        barraMenu.current.style.width = "150px"
+        barraMenu.current.style.transition = "1s"
+
+        txtIcon.current.style.transition = "2s"
+        txtIcon.current.style.color = "white"
+        txtIcon.current.style.right = "30px"
+
+        icon.current.style.backgroundColor = "rgb(24, 59, 105)"
+        icon.current.style.transition = "0.5s"
+    }
+
+    const cerrarMenu = () => {
+        barraMenu.current.style.width = "80px"
+        barraMenu.current.style.transition = "1s"
+        
+        txtIcon.current.style.color = "rgb(35, 82, 144)"
+        txtIcon.current.style.transition = "0.5s"
+
+        icon.current.style.backgroundColor = "rgb(35, 82, 144)"
+        icon.current.style.transition = "0.5s"
+    }
+
+    const generarToken = () => {
+        let request = "api/auth/login"
+        let url = Global.urlAlumnos + request
+        var user = {
+            userName: "admin@tajamar365.com",
+            password: "12345"
+        }
+        axios.post(url, user).then(response => {
+            localStorage.setItem('token', response.data.response)
+        })
+
+
+        
+        
+    }
+
+    return (
+        <div>
+            <div className='menu' onMouseEnter={ampliarMenu} onMouseLeave={cerrarMenu}>
+                <div className='barra-iconos' ref={barraMenu}>
+                    <div>
+                        <NavLink to='/'>
+                            <img src={logo} className='logo' />
+                        </NavLink>
+                    </div>
+                    
+                    <div className='icon-box' ref={icon}>
+                        <NavLink to='curso'>
+                            <img src={curso} className='icons' />
+                        </NavLink>
+                        <h4 className='txt-icon' ref={txtIcon}>Curso</h4>
+                    </div>
+                    <div className='icon-box' ref={icon}>
+                        <button className='boton' onClick={generarToken}>Generear Token</button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    )
+  }
+
+
+export default Menu
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Menu from './Menu';
 import Home from './Home';
 import About from './About';
 import NotFound404 from './NotFound404';
 import Login from './Login/Login';
+import Menu from './Menu/Menu';
+import ListaUsuarios from './Curso/ListaUsuarios';
 
 const Router = () => {
     return (
         <BrowserRouter>
+        <Menu/>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/curso" element={<ListaUsuarios/>} />
                 <Route path="/about" element={<About />} />
                 <Route path="*" element={<NotFound404 />} />
             </Routes>
