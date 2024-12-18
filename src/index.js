f1da3336575214945f1589324e7eb19e971f6e4a
jjlagunamoreno <jjlagunamoreno@gmail.com>
2024-12-18 18:05:56 +0100

    FALTABA IMPORTACION EN INDEX

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import $ from 'jquery';
 import Popper from 'popper.js'
 import "bootstrap/dist/js/bootstrap.bundle";
+//PURO CSS RICO RICO RICO
+import { MantineProvider } from '@mantine/core';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -12,7 +14,9 @@ import Router from './components/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router />
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <Router />
+  </MantineProvider>
 );
 
 reportWebVitals();
